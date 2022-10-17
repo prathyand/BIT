c90de1450b0c9f47b455857a07664c8f2b12512d
pdeshmuk <pdeshmuk@iu.edu>
2022-10-17 08:43:22 -0400

    auth module added

---
+++ auth_service/constants.js
@@ -0,0 +1,8 @@
+// const dotenv = require('dotenv');
+// dotenv.config();
+
+module.exports = {
+    DB_HOSTS: process.env.DB_HOSTS || 'localhost:27017',
+    DB_DATABASE_AUTH: process.env.DB_DATABASE || 'authDB',
+    APP_PORT: process.env.APP_PORT || 3001,
+}
