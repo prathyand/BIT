79da14985c70eace56cc44f5992c0a95ed11f378
pdeshmuk <pdeshmuk@iu.edu>
2022-10-19 07:19:08 -0400

    cleaning old files

--- src/backend/login/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
-
-def say_hello(request):
-    # return HttpResponse("Hello World")
-    return render(request, 'hello.html', {'name':'username_here'})
