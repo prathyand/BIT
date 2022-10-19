a40f87ee62c5e2651f1d196e97791061797d40ba
bjmcshane <bmcshane@iu.edu>
2022-10-18 20:12:29 -0400

    cleaning up dev remote branch

--- auth_service/constants.js
+++
@@ -1,8 +0,0 @@
-// const dotenv = require('dotenv');
-// dotenv.config();
-
-module.exports = {
-    DB_HOSTS: process.env.DB_HOSTS || 'localhost:27017',
-    DB_DATABASE_AUTH: process.env.DB_DATABASE || 'authDB',
-    APP_PORT: process.env.APP_PORT || 3001,
-}
