fe1772bd461df6bb80a68022acd1869c787596b6
pdeshmuk <pdeshmuk@iu.edu>
2022-10-23 07:18:40 -0400

    added google auth/file structure changes

--- src/auth_service/constants.js
+++ src/auth_service/constants.js
@@ -1,8 +1,9 @@
-// const dotenv = require('dotenv');
-// dotenv.config();
+const dotenv = require('dotenv');
+dotenv.config();
 
 module.exports = {
     DB_HOSTS: process.env.DB_HOSTS || 'localhost:27017',
     DB_DATABASE_AUTH: process.env.DB_DATABASE || 'authDB',
     APP_PORT: process.env.APP_PORT || 3001,
+    frontend_google_clientID: process.env.GCLIENT_ID || " ",
 }
