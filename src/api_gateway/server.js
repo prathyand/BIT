2e1f1d5eafd5b173912d24b906d7f3c87523f087
bjmcshane <bmcshane@iu.edu>
2022-10-28 20:06:02 -0400

    framework for api gateway up

--- src/api_gateway/server.js
+++ src/api_gateway/server.js
@@ -1,9 +1,8 @@
-// const Joi = require('joi');
 const request = require('request');
 var express = require('express');
 var app = express();
 
-app.use(express.json()) // allows for json parsing (adding a piece of middleware)
+app.use(express.json()) 
 // const CONSTANTS = require("../../constants")
 const port = process.env.PORT || 3000;
 
@@ -11,37 +10,101 @@ const port = process.env.PORT || 3000;
 // const router = express.Router();
 app.all('/*', (req, res, next) => {
     //console.log(req)
-    const path = req.path
-    const body = req.body
-    const method = req.method
-    console.log(path)
+    const incoming_path = req.path
+    const incoming_body = req.body
+    const incoming_method = req.method
+    const incoming_hostname = req.hostname
+    const incoming_hostip = req.ip
+    const incoming_protocol = req.protocol
+    const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+    console.log(path) // /
+    console.log(body) // {}
+    console.log(method) // GET
+    console.log(req.protocol) //http
+    console.log(req.ip) // ::1
+    console.log(req.hostname)  // localhost
+    console.log(req.baseUrl) // *blank*
     console.log(body)
-    console.log(method)
+    console.log(fullUrl)
 
     // var response = request(path, )
     // res.send(response)
-
-    const options = {
+    var options = {
+        url: 'https://www.reddit.com/r/funny.json',
+        method: incoming_method,
+        headers: {
+            'Accept': 'application/json',
+            'Accept-Charset': 'utf-8',
+            'User-Agent': 'my-reddit-client'
+        },
+        json: true,
+        body: incoming_body,
+        time: true
+    }
+    var options = {
         url: 'https://www.reddit.com/r/funny.json',
         method: 'GET',
         headers: {
             'Accept': 'application/json',
             'Accept-Charset': 'utf-8',
             'User-Agent': 'my-reddit-client'
-        }
+        },
+        json: true,
+        body: 'hello world',
+        time: true
     }
 
-    var response = request(options, (err, res, body) => {
-        var json = JSON.parse(body);
-        // console.log(response)
-        console.log(json.kind)
-        return json
+    request(options, (err, res, body) => {
+        if (!err && res.statusCode == 200) {
+            console.log('success!')
+
+            // var json = JSON.parse(body);
+            // console.log(response)
+            // console.log(json.kind)
+            //return json
+        }
+        else {
+            console.log(err)
+            res.status(400).json({
+                message: "incorrect http request"
+            })
+        }
     });
-    console.log(response)
-    res.send(response)
 
     console.log('done')
     next();
+    
+    // need to pick apart response from Docker microservices, to send info to front end container
+    // var options = {
+        // url: 'https://www.reddit.com/r/funny.json',
+        // method: 'GET',
+        // headers: {
+            // 'Accept': 'application/json',
+            // 'Accept-Charset': 'utf-8',
+            // 'User-Agent': 'my-reddit-client'
+        // },
+        // json: true,
+        // body: 'hello world',
+        // time: true
+    // }
+
+    
+    // var response = request(options, (err, res, body) => {
+        // if (!err && res.statusCode == 200) {
+            // console.log('success!')
+
+            // // var json = JSON.parse(body);
+            // // console.log(response)
+            // // console.log(json.kind)
+            // //return json
+        // }
+        // else {
+            // console.log(err)
+            // res.status(400).json({
+                // message: "incorrect http request"
+            // })
+        // }
+    // });
 });
 
 // app.use('/', router);
