0077d8d1bb61f5d54e48e86202766b6a53fc8247
bjmcshane <bmcshane@iu.edu>
2022-10-30 21:32:23 -0400

    trying to find post body bug

--- src/api_gateway/server.js
+++ src/api_gateway/server.js
@@ -1,9 +1,17 @@
 const request = require('request');
 var express = require('express');
+const bodyParser = require("body-parser");
 var app = express();
 app.use(express.json()) 
 const CONSTANTS = require("./constants")
 
+app.use(bodyParser.json());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();
+});
 
 function forward_auth_req(req, res, path) {
     const outboundUrl = req.protocol + '://' + CONSTANTS.CONTAINER_HOSTNAME + ':' + CONSTANTS.AUTH_PORT + path
@@ -19,10 +27,10 @@ function forward_auth_req(req, res, path) {
         json: true,
     }
 
-    console.log(options)
 
     request(options, (err, res2, body) => {
         // console.log(res2)
+        console.log(options)
         if (!err && res.statusCode == 200) {
             res.send(res2)
         }
@@ -37,25 +45,24 @@ function forward_auth_req(req, res, path) {
 }
 
 
-
 app.use("/login", (req, res) => {
     console.log(req.body)
     forward_auth_req(req, res, "/login");
 });
 
-app.use("/signup", (req, res, next) => {
+app.use("/signup", (req, res) => {
     forward_auth_req(req, res, "/signup");
 });
 
-app.use("/profile", (req, res, next) => {
+app.use("/profile", (req, res) => {
     forward_auth_req(req, res, "/profile");
 });
 
-app.use("/updateprofile", (req, res, next) => {
+app.use("/updateprofile", (req, res) => {
     forward_auth_req(req, res, "updateprofile");
 });
 
-app.use("/auth/google", (req, res, next) => {
+app.use("/auth/google", (req, res) => {
     forward_auth_req(req, res, "/auth/google");
 });
 
