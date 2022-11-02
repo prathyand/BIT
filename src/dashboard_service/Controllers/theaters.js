17e924fd18fe8b6cb75b5bfda19c01e3f49cfd42
bjmcshane <bmcshane@iu.edu>
2022-11-02 14:42:14 -0400

    slight dashboard service changes

--- src/dashboard_service/Controllers/theaters.js
+++ src/dashboard_service/Controllers/theaters.js
@@ -64,27 +64,6 @@ const getZipTheaters = (async (req, res) => {
     }
 });
 
-// router.get('/city/:cityName',auth, 
-    // async (req,res)=>{
-        // try{
-            // // grab all movies for city 'cityName'
-            // const theaterlist = await Theater.find({'City':req.params['cityName']}).select({'showings.movies.id':1,'_id':0});
-            // const movieidSet = new Set()
-            // for(var i=0;i<theaterlist.length;i++){
-                // for(var j=0;j<theaterlist[i]['showings']['movies'].length;j++){
-                    // movieidSet.add(theaterlist[i]['showings']['movies'][j]['id']);
-                // }
-            // }
-            
-            // const moviesList = await Movie.find({ _id: { $in: Array.from(movieidSet)} });
-            // return res.status(200).json({movieidList:moviesList});
-
-        // }catch(err){
-            // console.log(err);
-            // return res.status(400).send('Server Error!');
-        // }
-// });
-
 // grabs all theaters playing a movie (by movie id)
 const getMoviesTheaters = (async (req, res) => {
     try {
