64b5fc200ad56b37f2dcb3e573340d79d8706e3f
pdeshmuk <pdeshmuk@iu.edu>
2022-11-03 15:57:12 -0400

    added gateway port

--- src/frontend/src/constants.js
+++ src/frontend/src/constants.js
@@ -7,7 +7,7 @@
 
 module.exports = {
     GATEWAY1: process.env.GATEWAY1 || 'localhost',
-    GATEWAY1_PORT: process.env.GATEWAY1_PORT || 3001,
+    GATEWAY1_PORT: process.env.GATEWAY1_PORT || 5001,
     DOMAIN: process.env.FEDOMAIN || 'localhost',
     FE_PORT: process.env.FEPORT || 3000,
     CLIENT_ID: process.env.CLIENT_ID || "624148654388-pep2rsbqfuscb5mibsh16l3fv1hhlp7e.apps.googleusercontent.com"
