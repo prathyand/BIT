e329e1f558c528895c4feb1aaa706d6c27278668
pdeshmuk <pdeshmuk@iu.edu>
2022-11-03 08:53:03 -0400

    added /movies/zip and /movies/theater

--- src/dashboard_service/Routes/movies.js
+++ src/dashboard_service/Routes/movies.js
@@ -37,20 +37,48 @@ router.get('/city/:cityName',auth,
 router.get('/theater/:theaterId',auth, 
     async (req,res)=>{
         try{
-            // grab all movies for theater 'theaterId'
-            const movieList = await Theater.find({'_id':req.params['theaterId']}).select({'showings.movies':1,'_id':0});
-            // console.log(movieList[0]['showings']['movies']);
-            const movieidSet = []
-            // console.log(movieList);
-            for(var j=0;j<movieList[0]['showings']['movies'].length;j++){
-                movieidSet.push(movieList[0]['showings']['movies'][j]['id']);
+            // grab the theater with theater id 'theaterId'
+            let theater = await Theater.findById({'_id':req.params['theaterId']}).select({'showings.movies':1,'_id':0});
+            theater=theater.toObject();
+
+            const movieidSet = [];
+          
+            for(var j=0;j<theater['showings']['movies'].length;j++){
+                movieidSet.push(theater['showings']['movies'][j]['id']);
             }
             
-            const moviesList = await Movie.find({ _id: { $in: movieidSet} });
-            for(var i=0;i<movieidSet.length;i++){
-                moviesList[i]["timings"]=Array.from(movieList[0]['showings']['movies'][i]['timings']);
+            let moviesList = await Movie.find({ _id: { $in: movieidSet} });
+
+            moviesList=moviesList.map((item,ind)=>{
+                item=item.toObject();
+                item.timings=theater['showings']['movies'][ind]['timings'];
+                return item;});
+
+            return res.status(200).send({moviesList});
+
+        }catch(err){
+            console.log(err);
+            return res.status(400).send('Server Error!');
+        }
+});
+
+
+// sample usage: 
+// get the list of all the movies for zip 'zipcode':
+// GET http://dashboard_service:3002/movies/zip/46202
+router.get('/zip/:zipcode',auth, 
+    async (req,res)=>{
+        try{
+            // grab all movies for zip 'zipcode'
+            const theaterlist = await Theater.find({'Zip':req.params['zipcode']}).select({'showings.movies.id':1,'_id':0});
+            const movieidSet = new Set();
+            for(var i=0;i<theaterlist.length;i++){
+                for(var j=0;j<theaterlist[i]['showings']['movies'].length;j++){
+                    movieidSet.add(theaterlist[i]['showings']['movies'][j]['id']);
+                }
             }
             
+            const moviesList = await Movie.find({ _id: { $in: Array.from(movieidSet)} });
             return res.status(200).json({movieidList:moviesList});
 
         }catch(err){
