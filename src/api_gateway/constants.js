5312659b6a538c81b9959998b5839c87664962bd
pdeshmuk <pdeshmuk@iu.edu>
2022-11-03 12:31:01 -0400

    removed dotenv

--- src/api_gateway/constants.js
+++ src/api_gateway/constants.js
@@ -1,6 +1,3 @@
-const dotenv = require('dotenv');
-dotenv.config();
-
 module.exports = {
     AUTH_CONTAINER_HOSTNAME: process.env.AUTH_CONTAINER_HOSTNAME || 'localhost',
     AUTH_PORT: process.env.AUTH_PORT || '3001',
