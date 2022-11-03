c68e10b829c758140326b24a44be5e152caf9b52
bjmcshane <bmcshane@iu.edu>
2022-11-03 12:06:18 -0400

    moved dashboard_service to controller framework, untested

--- src/dashboard_service/Controllers/theaters.js
+++ src/dashboard_service/Controllers/theaters.js
@@ -1,12 +1,11 @@
 const Theater = require("../Models/Theater");
 const Movie = require("../Models/Movies");
-const auth = require("../Middleware/auth");
 const { json } = require("express");
 
 // grabs all unique theater names from Theater data table
 const getTheaters = (async (req, res) => {
     try{
-        const theatersList = await Theater.distinct('name');
+        const theatersList = await Theater.find().select({'name':1, '_id':1});
         return res.status(200).json({theaters: theatersList});
 
     } catch(err) {
@@ -14,28 +13,6 @@ const getTheaters = (async (req, res) => {
         return res.status(400).send('Server Error!');
     }
 });
-
-
-// router.get('/city/:cityName',auth, 
-    // async (req,res)=>{
-        // try{
-            // // grab all movies for city 'cityName'
-            // const theaterlist = await Theater.find({'City':req.params['cityName']}).select({'showings.movies.id':1,'_id':0});
-            // const movieidSet = new Set()
-            // for(var i=0;i<theaterlist.length;i++){
-                // for(var j=0;j<theaterlist[i]['showings']['movies'].length;j++){
-                    // movieidSet.add(theaterlist[i]['showings']['movies'][j]['id']);
-                // }
-            // }
-            
-            // const moviesList = await Movie.find({ _id: { $in: Array.from(movieidSet)} });
-            // return res.status(200).json({movieidList:moviesList});
-
-        // }catch(err){
-            // console.log(err);
-            // return res.status(400).send('Server Error!');
-        // }
-// });
 
 // gets all theaters in a specific city
 const getCityTheaters = (async (req, res) => {
--- src/dashboard_service/Routes/cities.js
+++ src/dashboard_service/Routes/cities.js
@@ -4,19 +4,9 @@ const Theater = require("../Models/Theater");
 // no need for authentication middleware
 
 
-// return the list of all cities in the database
-router.get('/', 
-    async (req,res)=>{
-        
-        try{
-            // grab all unique Theater['City'] from db
-            const citiesList = await Theater.distinct('City');
-            return res.status(200).json({'cities':citiesList});
+const {getCities} = require("../Controllers/cities")
 
-        }catch(err){
-            console.log(err);
-            return res.status(400).send('Server Error!');
-        }
-});
+// return the list of all cities in the database
+router.get('/', getCities);
 
 module.exports = router;
