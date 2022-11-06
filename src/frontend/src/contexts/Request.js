6f5d1e88e69d98db14d858732abcdb6913bbf0e7
pdeshmuk <pdeshmuk@iu.edu>
2022-11-06 18:17:05 -0500

    changes to ENV variables/new dockerfile

--- src/frontend/src/contexts/Request.js
+++ src/frontend/src/contexts/Request.js
@@ -9,7 +9,7 @@ const Request = React.createContext({
 });
 
 export const RequestProvider = (props)=>{
-    const domainName_gw1 = "http://" + constants.GATEWAY1 + ":" + constants.GATEWAY1_PORT;
+    const domainName_gw1 = "http://" + constants.REACT_APP_GATEWAY + ":" + constants.REACT_APP_GATEWAY1_POR;
     const webDomain = "http://" + constants.DOMAIN + ":" + constants.FE_PORT;
     const authContext = useContext(AuthContext)
     const fetchRequest =  (method,url,reqBody,reqHeader) => {
