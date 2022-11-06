68828e5f9d5d1ef213e7e0f9534888dd0d749b75
pdeshmuk <pdeshmuk@iu.edu>
2022-11-06 11:29:49 -0500

    added basic tests

--- src/auth_service/App.js
+++ src/auth_service/App.js
@@ -28,8 +28,18 @@ app.use(function (req, res, next) {
   app.use("/updateprofile", updateprofileRoute);
   app.use("/auth/google", gloginRoute);
   
+  app.get("/ping", (req, res) => {
+    res.json({
+      ping: "pokemon"
+    });
+  });
+
 
   const server = app.listen(CONSTANTS.APP_PORT, () => {
     console.log(`Server is running at port ${CONSTANTS.APP_PORT}`);
+    if (process.env.NODE_ENV != 'test') {
       connectDB();
+    }
   });
+
+  module.exports = { app, server };
