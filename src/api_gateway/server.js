83a8df035a5e92760a43470feae33e4c2043d3ca
bjmcshane <bmcshane@iu.edu>
2022-12-06 17:48:07 -0600

    customer lookup almost done

--- src/api_gateway/server.js
+++ src/api_gateway/server.js
@@ -82,6 +82,10 @@ router.post('/bookings', (req, res,next) => {
     dashboardServerProxy(req,res,next);  
 });
 
+router.get('/bookings', (req, res,next) => {
+    dashboardServerProxy(req,res,next);  
+});
+
 router.get('/gettheaterinfo', (req, res,next) => {
     dashboardServerProxy(req,res,next);  
 });
