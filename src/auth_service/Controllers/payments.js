a7ee6d1351c41f683c298b10f81f9383afac29cd
pdeshmuk <pdeshmuk@iu.edu>
2022-12-10 02:23:08 -0500

    changed redirect url

--- src/auth_service/Controllers/payments.js
+++ src/auth_service/Controllers/payments.js
@@ -3,7 +3,7 @@ const CONSTANTS = require("../constants");
 
 let YOUR_DOMAIN = 'http://localhost/';
 if(CONSTANTS.environment!="docker"){
-  YOUR_DOMAIN="http://149.165.169.69/"
+  YOUR_DOMAIN="http://149.165.169.69.nip.io/"
 }
 
 const payment = async (req, res) => {
